b1feb367b19a6b3e9a4d161345fe05679a90a666
tarazan <tarazan@tarazan.tarazan>
2024-04-05 00:13:34 +0200

    reinstall the app with yarn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+  reactStrictMode: true,
+  publicRuntimeConfig: {
+    imagesFolder: 'static/images',
+  },
+};
 
 export default nextConfig;
