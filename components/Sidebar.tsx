62fa63767340dcc8de8a1d097817cf166790b24c
tarazan <tarazan@tarazan.tarazan>
2024-04-09 00:19:56 +0200

    finished video chat app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import { sidebarLinks } from '@/constants';
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import SidebarLinks from './SidebarLinks';
 
@@ -13,7 +9,7 @@ export default function Sidebar() {
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
       <div className='flex flex-1 flex-col gap-6'>
-        <SidebarLinks />
+        <SidebarLinks location='' />
       </div>
     </section>
   );
