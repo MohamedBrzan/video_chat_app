76e89725371d2fda29b8a2210eb4bdc548f53541
tarazan <tarazan@tarazan.tarazan>
2024-04-11 06:42:37 +0200

    update yoom project meeting room className for responsive

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -43,16 +43,16 @@ const MeetingRoom = () => {
       case 'grid':
         return <PaginatedGridLayout />;
       case 'speaker-right':
-        return <SpeakerLayout participantsBarPosition="left" />;
+        return <SpeakerLayout participantsBarPosition='left' />;
       default:
-        return <SpeakerLayout participantsBarPosition="right" />;
+        return <SpeakerLayout participantsBarPosition='right' />;
     }
   };
 
   return (
-    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
-      <div className="relative flex size-full items-center justify-center">
-        <div className=" flex size-full max-w-[1000px] items-center">
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+      <div className='relative flex size-full items-center justify-center'>
+        <div className=' flex size-full max-w-[1000px] items-center'>
           <CallLayout />
         </div>
         <div
@@ -64,16 +64,16 @@ const MeetingRoom = () => {
         </div>
       </div>
       {/* video layout and call controls */}
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap p-2'>
         <CallControls onLeave={() => router.push(`/`)} />
 
         <DropdownMenu>
-          <div className="flex items-center">
-            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
-              <LayoutList size={20} className="text-white" />
+          <div className='flex items-center'>
+            <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  '>
+              <LayoutList size={20} className='text-white' />
             </DropdownMenuTrigger>
           </div>
-          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+          <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
             {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
               <div key={index}>
                 <DropdownMenuItem
@@ -83,15 +83,15 @@ const MeetingRoom = () => {
                 >
                   {item}
                 </DropdownMenuItem>
-                <DropdownMenuSeparator className="border-dark-1" />
+                <DropdownMenuSeparator className='border-dark-1' />
               </div>
             ))}
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
         <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
-            <Users size={20} className="text-white" />
+          <div className=' cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  '>
+            <Users size={20} className='text-white' />
           </div>
         </button>
         {!isPersonalRoom && <EndCallButton />}
